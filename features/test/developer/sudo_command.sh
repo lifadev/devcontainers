8d89af02a935e8311564f948a6f0303faf0ccc76
Farzad Senart <farzad.senart@gmail.com>
2026-01-25 14:46:17 +0000

    maintainability: shelldre:SS1192,S7682
    Signed-off-by: Farzad Senart <farzad.senart@gmail.com>

--- features/test/developer/sudo_command.sh
+++ features/test/developer/sudo_command.sh
@@ -3,8 +3,9 @@
 set -eux
 
 source dev-container-features-test-lib
+OK="ok" KO="ko"
 
-check allowed test "$(sudo echo >/dev/null 2>&1 && echo "expected" || echo "unexpected")" = "expected"
-check restricted test "$(sudo ls >/dev/null 2>&1 && echo "unexpected" || echo "expected")" = "expected"
+check allowed test "$(sudo echo >/dev/null 2>&1 && echo $OK || echo $KO)" = $OK
+check restricted test "$(sudo ls >/dev/null 2>&1 && echo $KO || echo $OK)" = $OK
 
 reportResults
