38e28076c2a31b379309f703886e91d1be4f1137
Farzad Senart <farzad.senart@gmail.com>
2026-01-25 13:49:53 +0000

    reliability: fix shelldre:S7688
    Signed-off-by: Farzad Senart <farzad.senart@gmail.com>

--- features/lib/install.sh
+++ features/lib/install.sh
@@ -2,13 +2,13 @@
 
 set -eux
 
-if [ "$UID" -ne 0 ]; then
+if [[ "$UID" -ne 0 ]]; then
   echo -e "(!) User must be root: $UID"
   exit 1
 fi
 
 ARCH="$(dpkg --print-architecture)"
-if [ "$ARCH" != 'amd64' ] && [ "$ARCH" != 'arm64' ]; then
+if [[ "$ARCH" != 'amd64' ]] && [[ "$ARCH" != 'arm64' ]]; then
   echo "(!) Unsupported architecture: $ARCH"
   exit 1
 fi
