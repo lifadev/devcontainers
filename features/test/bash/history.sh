8ede430c9fda5fa06b46f6d37098b94706599f44
Farzad S. <farzad.senart@gmail.com>
2026-01-25 19:10:36 -0500

    feature/bash: standardize mount permissions (#1770)
    Signed-off-by: Farzad Senart <farzad.senart@gmail.com>

--- features/test/bash/history.sh
+++ features/test/bash/history.sh
@@ -4,7 +4,9 @@ set -eux
 
 source dev-container-features-test-lib
 
-check "owner" test "$(stat -c %U -- "$HISTFILE")" = "developer"
-check "group" test "$(stat -c %G -- "$HISTFILE")" = "developer"
+BASH_CONFIG_DIR=$(dirname "$HISTFILE")
+
+check "owner" test "$(stat -c %U -- "$BASH_CONFIG_DIR")" = "developer"
+check "group" test "$(stat -c %G -- "$BASH_CONFIG_DIR")" = "developer"
 
 reportResults
