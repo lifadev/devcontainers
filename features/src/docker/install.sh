8233d112a6f53c6147d236caf501c441d698d4d1
Farzad S. <farzad.senart@gmail.com>
2026-02-01 22:48:49 -0500

    features: add docker (#1788)
    Signed-off-by: Farzad Senart <farzad.senart@gmail.com>

---
+++ features/src/docker/install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -eux
+
+# shellcheck source=../../lib/install.sh
+source dev-container-features-install-lib
+
+dc_install \
+  iptables
+
+INIT_DIR=$(dc_mkdir /opt/init)
+cp init.sh "$INIT_DIR/docker.sh"
+chmod 0550 "$INIT_DIR/docker.sh"
+
+PACKAGE=/tmp/package.tgz
+dc_download cli $PACKAGE
+
+INSTALL_DIR=$(dc_mkdir /opt/bin)
+tar -xzf $PACKAGE --strip-components 1 -C "$INSTALL_DIR"
+
+dc_bash_complete docker <<EOF
+eval "\$($INSTALL_DIR/docker completion bash)"
+EOF
+
+PLUGINS_DIR=$(dc_mkdir /usr/local/libexec/docker/cli-plugins)
+
+PLUGIN=$PLUGINS_DIR/docker-buildx
+dc_download buildx "$PLUGIN"
+chmod +x "$PLUGIN"
+
+PLUGIN=$PLUGINS_DIR/docker-compose
+dc_download compose "$PLUGIN"
+chmod +x "$PLUGIN"
+
+groupadd docker
+usermod --append \
+  --groups docker \
+  "$_REMOTE_USER"
+
+RUN_DIR=$(dirname "${DOCKER_HOST#unix://}")
+dc_mkdir "$RUN_DIR"
